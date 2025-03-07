4f1f4ac00da23a34dbf08d92fb02a4e70e0b5bf8
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-03-07 12:14:05 +0100

    feat: noise feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Noise from "./components/noise";
+
 export default function App() {
   return (
     <div
-      className="h-screen overflow-y-scroll snap-y snap-mandatory"
+     className="h-screen overflow-y-scroll snap-y snap-mandatory"
       style={{
         scrollbarWidth: "none" /* Firefox */,
         msOverflowStyle: "none" /* IE and Edge */,
@@ -15,14 +17,17 @@ export default function App() {
           }
         `}
       </style>
-      <div className="w-full h-screen bg-red-500 flex items-center justify-center snap-start">
-        Panel 1
-      </div>
-      <div className="w-full h-screen bg-blue-500 flex items-center justify-center snap-start">
-        Panel 2
-      </div>
-      <div className="w-full h-screen bg-green-500 flex items-center justify-center snap-start">
-        Panel 3
+      <Noise />
+      <div className="relative z-20">
+        <div className="w-full h-screen bg-red-500 flex items-center justify-center snap-start">
+          Panel 1
+        </div>
+        <div className="w-full h-screen bg-blue-500 flex items-center justify-center snap-start">
+          Panel 2
+        </div>
+        <div className="w-full h-screen bg-green-500 flex items-center justify-center snap-start">
+          Panel 3
+        </div>
       </div>
     </div>
   );
